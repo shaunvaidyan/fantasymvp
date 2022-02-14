228ae7c7e5d0e30c55e56c427f93eff5162851bc
shaun <svaidyan24@gmail.com>
2022-02-14 12:53:42 -0600

    table added

--- src/lib/scriptHelper.js
+++ src/lib/scriptHelper.js
@@ -1,23 +1,37 @@
 //let leagueId = 787027217543708672; // build function to pass League ID from form later
+
+
+function formSubmission(document, leagueId, sleeperData, teams) {
+
+  //let leagueId = leagueUrl.value;
+
+  //reset visual elements of form
+  sleeperData.style.visibility = "visible";
+  //console.log(teams)
+}
+
 function addTeamInfo(document, owner, players){
   let sleeperData = document.getElementById("sleeperData");
-  sleeperData.innerHTML =
-    `<h2>Teams</h2>
-    <ol>
-        <li>owner: ${owner}</li>
-        <li>players: ${players}</li>
-    </ol>`
+  let ownerData = document.getElementById("ownerData");
+  for (let i=0; owner.length > i; i++){
+    ownerData.innerHTML +=
+    `<td>${owner[i]}</td>`
+    }
+     
+  // sleeperData.innerHTML =
+  //   `<h2>Teams</h2>
+  //   <ol>
+  //       <li>Owners: ${owner}</li>
+  //       <li>Players: ${players}</li>
+  //   </ol>`
 }
 async function myFetch() {
-  let leagueId = 787027217543708672;
-  let teamsReturned;
-
-  teamsReturned = await fetch(`https://api.sleeper.app/v1/league/787027217543708672/rosters`).then( function(response) {
+  let leagueId = "787027217543708672";
+  teamsReturned = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/rosters`).then( function(response) {
       return response.json();
   });
   return teamsReturned;
 }
-
 
 // fetch(`https://api.sleeper.app/v1/league/${leagueID}`)
 //   .then(function() {
--- src/lib/scripts.js
+++ src/lib/scripts.js
@@ -1,30 +1,28 @@
 window.addEventListener("load", function (){
 
-    let list = document.getElementById("teams");
-    list.style.visibility = 'hidden';
+    let sleeperData = document.getElementById("sleeperData");
+    sleeperData.style.visibility = 'hidden';
 
     let form = document.querySelector("form");
     form.reset(); // reset form and clear values if page refreshed
     form.addEventListener("submit", function(event){
 
-        let teams = document.querySelector("input[name=leagueUrl]").value;
-
-        formSubmission(document, list, teams);
+        var leagueId = document.querySelector("input[name=leagueUrl]").value;
+        console.log(leagueId);
+        formSubmission(document, leagueId, sleeperData, teams);
         event.preventDefault();
     });
    let listedTeams;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   // Set listedTeamsResponse equal to the value returned by calling myFetch()
    let listedTeamsResponse = myFetch();
    listedTeamsResponse.then(function (result) {
         listedTeams = result;
         console.log(listedTeams);
    }).then(function () {
     console.log(listedTeams);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
         
         let owner = listedTeams.map (o => o.owner_id);
         let players = listedTeams.map (o => o.players);
-
         addTeamInfo(document, owner, players);
    })
 
