8cb57695a2226cc805721defe07e75133c82afd7
shaun <svaidyan24@gmail.com>
2022-02-19 12:05:17 -0600

    implementing username check

--- src/lib/scriptHelper.js
+++ src/lib/scriptHelper.js
@@ -1,23 +1,75 @@
 //let leagueId = 787027217543708672; // build function to pass League ID from form later
 
 
-function formSubmission(document, leagueId, sleeperData, teams) {
+function formLeagueIdSubmission(document, leagueId, sleeperData, teams) {
 
   //let leagueId = leagueUrl.value;
 
   //reset visual elements of form
+
   sleeperData.style.visibility = "visible";
   //console.log(teams)
 }
+function buttonSubmission(document, leagueId, sleeperData, teams) {
 
-function addTeamInfo(document, owner, namedTeams){
+  //let leagueId = leagueUrl.value;
+
+  //reset visual elements of form
+
+  sleeperData.style.visibility = "visible";
+  //console.log(teams)
+}
+function addTeamInfoByLeagueUrl(document, owner, namedTeams, jsonLeagueId){
   let sleeperData = document.getElementById("sleeperData");
   let ownerData = document.getElementById("ownerData");
+  ownerData.innerHTML = `` //resets innerHTML table
   //let teamData = document.getElementById("teamData");
   for (let i=0; owner.length > i; i++){
     ownerData.innerHTML +=
-    `<td>${owner[i]}</td>
-    <td>${namedTeams[i]}</td>`
+    `<td><button name="${owner[i]}" id="${owner[i]}" type="submit" value="${owner[i]}">${owner[i]}</button></td>
+    <td><a href="#${namedTeams[i]}" onclick="RedirectURL();return false;">${namedTeams[i]}</a></td>`
+    // teamData.innerHTML +=
+    // `<td>${team[i]}</td>`
+    }
+     
+  // sleeperData.innerHTML =
+  //   `<h2>Teams</h2>
+  //   <ol>
+  //       <li>Owners: ${owner}</li>
+  //       <li>Players: ${players}</li>
+  //   </ol>`
+}
+
+function addTeamInfoByUserName(document, owner, namedTeams, jsonLeagueId){
+  let sleeperData = document.getElementById("sleeperData");
+  let ownerData = document.getElementById("ownerData");
+  ownerData.innerHTML = `` //resets innerHTML table
+  //let teamData = document.getElementById("teamData");
+  for (let i=0; owner.length > i; i++){
+    ownerData.innerHTML +=
+    `<td><button name="${owner[i]}" id="${owner[i]}" type="submit" value="${owner[i]}">${owner[i]}</button></td>
+    <td><a href="#${namedTeams[i]}" onclick="RedirectURL();return false;">${namedTeams[i]}</a></td>`
+    // teamData.innerHTML +=
+    // `<td>${team[i]}</td>`
+    }
+     
+  // sleeperData.innerHTML =
+  //   `<h2>Teams</h2>
+  //   <ol>
+  //       <li>Owners: ${owner}</li>
+  //       <li>Players: ${players}</li>
+  //   </ol>`
+}
+
+function addRosterInfo(document, playerOnRoster){
+  let sleeperData = document.getElementById("sleeperData");
+  let ownerData = document.getElementById("ownerData");
+  ownerData.innerHTML = `` //resets innerHTML table
+  //let teamData = document.getElementById("teamData");
+  for (let i=0; owner.length > i; i++){
+    ownerData.innerHTML +=
+    `<td><button name="${owner[i]}" type="submit" value="${owner[i]}">${owner[i]}</button></td>
+    <td><a href="#${namedTeams[i]}" onclick="RedirectURL();return false;">${namedTeams[i]}</a></td>`
     // teamData.innerHTML +=
     // `<td>${team[i]}</td>`
     }
@@ -31,11 +83,28 @@ function addTeamInfo(document, owner, namedTeams){
 }
 async function myFetchUsers() {
   let leagueId = document.querySelector("input[name=leagueUrl]").value; 
-  teamsReturned = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`).then( function(response) {
+  let teamsReturned = await fetch(`https://api.sleeper.app/v1/league/${leagueId}/users`).then( function(response) {
       return response.json();
   });
   return teamsReturned;
+  
 }
+function addRosterInfo(document, owner, namedTeams, jsonLeagueId){
+  
+
+}
+async function myFetchPlayers() {
+  //let selectedTeamPlayers = document.querySelector("").value; // GET ON THIS
+  let playersReturned = await fetch(`./nfl.json`).then( function(response) {
+      return response.json();
+  });
+  return playersReturned;
+}
+function RedirectURL(){
+    window.location= createDynamicURL();
+}
+
+
 
 // fetch(`https://api.sleeper.app/v1/league/${leagueID}`)
 //   .then(function() {
--- src/lib/scripts.js
+++ src/lib/scripts.js
@@ -6,24 +6,45 @@ window.addEventListener("load", function (){
     let form = document.querySelector("form");
     form.reset(); // reset form and clear values if page refreshed
     form.addEventListener("submit", function(event){
-
-        var leagueId = document.querySelector("input[name=leagueUrl]").value;
-        console.log(leagueId);
-        formSubmission(document, leagueId, sleeperData, teams);
+        //form.reset();
+        let leagueId = document.querySelector("input[name=leagueUrl]").value;
+        //console.log(leagueId);
+        formLeagueIdSubmission(document, leagueId, sleeperData, teams);
         event.preventDefault();
         let listedTeams;
    // Set listedTeamsResponse equal to the value returned by calling myFetchUsers()
         let listedTeamsResponse = myFetchUsers();
         listedTeamsResponse.then(function (result) {
         listedTeams = result;
-        console.log(listedTeams);
+
          }).then(function () {
-        console.log(listedTeams);
+        //console.log(listedTeams);
         
         let owner = listedTeams.map (o => o.display_name);
         let namedTeams = listedTeams.map (o => o.metadata.team_name);
-        addTeamInfo(document, owner, namedTeams);
+        let jsonLeagueId = listedTeams.map (o => o.league_id);
+        addTeamInfoByLeagueUrl(document, owner, namedTeams, jsonLeagueId);
         })
-        });
+    });
+
+    // let button = document.querySelector("button");
+    // button.addEventListener("click", function(event){
+    //     let rosterId;
+        
+    //     buttonSubmission(document, rosterId, sleeperData, teams);
+    //     console.log(rosterId);
+    //     event.preventDefault();
+    //     let listedPlayers;
+    //     let listedPlayersResponse = myFetchPlayers();
+    //     listedPlayersResponse.then(function (result){
+    //         listedPlayers = result;
+    //     }).then(function () {
+    //     console.log(listedPlayers);
+
+    //     let playerOnRoster = listedPlayers.map (o => o.full_name);
+    //     addRosterInfo(document, playerOnRoster);
+    //     })
+    // })
+
 
 });
